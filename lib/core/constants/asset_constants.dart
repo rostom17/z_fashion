8019f165ce52d73765bc0bf3e2181c03ea07c44a
rostom17 <rostom825@gmail.com>
2025-11-11 17:40:18 +0600

    UI (feat): bottom nav item added

---
+++ lib/core/constants/asset_constants.dart
@@ -0,0 +1,13 @@
+class AssetConstants {
+  static const _baseIconPath = "assets/icons";
+  static const chatFilledIcon = "$_baseIconPath/chat-filled.png";
+  static const chatIcon = "$_baseIconPath/chat.png";
+  static const heartFilledIcon = "$_baseIconPath/heart-filled.png";
+  static const heartIcon = "$_baseIconPath/heart.png";
+  static const homeIcon = "$_baseIconPath/home.png";
+  static const homeFilledIcon = "$_baseIconPath/home-filled.png";
+  static const shoppingBagFilledIocn = "$_baseIconPath/shopping-bag-filled.png";
+  static const shoppingBagIocn = "$_baseIconPath/shopping-bag.png";
+  static const userIcon = "$_baseIconPath/user.png";
+  static const userFilledIcon = "$_baseIconPath/user-filled.png";
+}
--- lib/features/main_navigation/presentation/widgets/bottom_nav_bar.dart
+++ lib/features/main_navigation/presentation/widgets/bottom_nav_bar.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:z_fashion/core/constants/asset_constants.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  void _onChanged() {}
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(bottom: 28, left: 28, right: 28),
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxHeight: 70),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.black87,
-            borderRadius: BorderRadius.circular(100),
-          ),
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.black87,
+          borderRadius: BorderRadius.circular(100),
         ),
+        child: Row(
+          children: [
+            _buildTabItem(
+              selectedIcon: AssetConstants.chatFilledIcon,
+              unSelectedIcon: AssetConstants.chatIcon,
+              itemIndex: 0,
+              currentIndex: 0,
+              context: context,
+            ),
+            _buildTabItem(
+              selectedIcon: AssetConstants.chatFilledIcon,
+              unSelectedIcon: AssetConstants.chatIcon,
+              itemIndex: 1,
+              currentIndex: 0,
+              context: context,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTabItem({
+    required String selectedIcon,
+    required String unSelectedIcon,
+    required int itemIndex,
+    required int currentIndex,
+    required BuildContext context,
+  }) {
+    final bool isSelected = itemIndex == currentIndex;
+    return Expanded(
+      child: InkWell(
+        onTap: () {},
+        child: ImageIcon(AssetImage(unSelectedIcon), color: Colors.white54),
       ),
     );
   }
